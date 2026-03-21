ae127e31e542797ab7f7b981f38e450d8aeced7e
Uddhav <uddhav3114@gmail.com>
2026-03-21 03:38:17 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/splash_screen.dart';
-import 'screens/settings_screen.dart';
 
 void main() {
   runApp(const MyApp());
