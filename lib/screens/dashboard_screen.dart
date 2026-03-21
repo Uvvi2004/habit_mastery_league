02fe695bd44d4280e9c5a9b649d9d561bafaa030
Uddhav <uddhav3114@gmail.com>
2026-03-20 20:25:59 -0400

    Connect dashboard to SQLite and display habits

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import '../database/db_helper.dart';
+import '../models/habit.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  List<Habit> habits = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadHabits();
+  }
+
+  Future<void> loadHabits() async {
+    final data = await DBHelper.instance.getHabits();
+    setState(() {
+      habits = data;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +31,27 @@ class DashboardScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Dashboard'),
       ),
-      body: const Center(
-        child: Text('Dashboard Screen'),
-      ),
+      body: habits.isEmpty
+          ? const Center(child: Text('No habits yet'))
+          : ListView.builder(
+              itemCount: habits.length,
+              itemBuilder: (context, index) {
+                final habit = habits[index];
+
+                return ListTile(
+                  title: Text(habit.name),
+                  subtitle: Text(habit.description),
+                  trailing: Icon(
+                    habit.isCompleted == 1
+                        ? Icons.check_circle
+                        : Icons.circle_outlined,
+                    color: habit.isCompleted == 1
+                        ? Colors.green
+                        : Colors.grey,
+                  ),
+                );
+              },
+            ),
     );
   }
 }
